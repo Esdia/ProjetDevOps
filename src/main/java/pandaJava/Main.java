4ae10f5bb1f23636c6b67397b063427e5b7abbe9
Esdia <theo.hermitte@protonmail.com>
2022-04-13 09:28:54 +0200

    Changed javadoc output directory

--- src/main/java/pandaJava/Main.java
+++ src/main/java/pandaJava/Main.java
@@ -1,6 +1,11 @@
 package pandaJava;
 
 public class Main {
+    /**
+     * Main method
+     *
+     * @param args Command line args
+     */
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
