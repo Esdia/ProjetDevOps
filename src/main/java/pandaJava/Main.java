0a39c2e539b47f05cdfed445f56e934dc0101438
Pierre <tournon.pierre1@gmail.com>
2022-04-18 15:33:19 +0200

    Tests pour le premier constructeur de DataFrame

--- src/main/java/pandaJava/Main.java
+++
@@ -1,7 +0,0 @@
-package pandaJava;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
--- src/test/java/pandaJava/DataframeTest.java
+++ src/test/java/pandaJava/DataframeTest.java
@@ -2,28 +2,81 @@ package pandaJava;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataframeTest {
 
-    private Dataframe dataframe;
     private final Object[][] correctArray = generateCorrectArray();
     private final Object[][] incorrectArray = generateIncorrectArray();
-
+    private final Dataframe d = new Dataframe(correctArray);
 
     @Test
-    public void test_incorrectArrayForConstructor () {
-
+    public void testValueFirstIndexOfFirstLine () {
+        assertEquals(0, d.getLine(0).get(0));
     }
 
-    @Test (expected = IndexOutOfBoundsException.class)
-    public boolean test () {
+    @Test
+    public void testValueFirstIndexOfFirstRow () {
+        assertEquals(0, d.getRow("A").get(0));
+    }
 
+    @Test
+    public void testSameValueFirstIndexRowAndLine () {
+        assertEquals(d.getLine(0).get(0), d.getRow("A").get(0));
+    }
+
+    @Test
+    public void testFirstLine () {
+        List<Integer> l = new ArrayList<Integer>() {
+            {
+                add(0);
+                add(1);
+                add(2);
+                add(3);
+                add(4);
+            }
+        };
+        Boolean ret = true;
+        for(int i = 0; i < l.size(); i++) {
+            if(!l.get(i).equals(d.getLine(0).get(i))) {
+                ret = false;
+            }
+        }
+        assertTrue(ret);
+    }
+
+    @Test
+    public void testFirstRow () {
+        List<Integer> l = new ArrayList<Integer>() {
+            {
+                add(0);
+                add(5);
+                add(10);
+                add(15);
+                add(20);
+            }
+        };
+        Boolean ret = true;
+        for(int i = 0; i < l.size(); i++) {
+            if(!l.get(i).equals(d.getRow("A").get(i))) {
+                ret = false;
+            }
+        }
+        assertTrue(ret);
+    }
+
+    @Test
+    public void testIncorrectArray () {
+        Dataframe d2 = new Dataframe(incorrectArray);
+        assertThrows(NullPointerException.class, () -> d2.getLine(0));
     }
 
     private Object[][] generateCorrectArray () {
         int nb = 0;
-        Object[][] test = new Object[3][3];
+        Object[][] test = new Object[5][5];
         for(int i = 0; i < 5; i++) {
             for(int j = 0; j < 5; j++) {
                 test[i][j] = nb;
@@ -35,7 +88,7 @@ class DataframeTest {
 
     private Object[][] generateIncorrectArray () {
         int nb = 0;
-        Object[][] test = new Object[3][3];
+        Object[][] test = new Object[5][5];
         for(int i = 0; i < 5; i++) {
             for(int j = 0; j < 5; j++) {
                 if(i == 0 && j==0)
@@ -47,9 +100,5 @@ class DataframeTest {
         }
         return test;
     }
-
-
-
-
 
 }
