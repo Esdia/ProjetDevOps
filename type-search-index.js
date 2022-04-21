4d4967c1601a6580b8a52db48946da5567c8def7
PierreTournon <73890001+PierreTournon@users.noreply.github.com>
2022-04-21 13:46:12 +0000

    Deploying to gh-pages from @ Esdia/ProjetDevOps@9673cb304f0bda20708f7f42a279a1117f0febb7 🚀

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"pandaJava","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"pandaJava","l":"Dataframe"},{"p":"pandaJava","l":"MistypedRowException"}]
