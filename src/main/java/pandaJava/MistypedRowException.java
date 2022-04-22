3d4c02d35106986e913404f1c01be74089ac821c
Pierre <tournon.pierre1@gmail.com>
2022-04-22 16:52:43 +0200

    Ajout de commentaires JavaDoc

--- src/main/java/pandaJava/MistypedRowException.java
+++ src/main/java/pandaJava/MistypedRowException.java
@@ -2,10 +2,18 @@ package pandaJava;
 
 public class MistypedRowException extends Exception {
 
+    /**
+     * Constructeur en fonction d'un message d'erreur et d'une erreur Throwable.
+     * @param errorMessage
+     * @param exception
+     */
     public MistypedRowException (String errorMessage, Throwable exception) {
         super(errorMessage, exception);
     }
 
+    /**
+     * Constructeur par défaut.
+     */
     public MistypedRowException () {
         super();
     }
