24110e575ad203931526aa6eb7508c89b8a33480
Esdia <theo.hermitte@protonmail.com>
2022-04-27 17:22:42 +0200

    Reformatted file

--- src/main/java/pandaJava/Dataframe.java
+++ src/main/java/pandaJava/Dataframe.java
@@ -16,6 +16,7 @@ public class Dataframe {
 
     /**
      * Création d'un DataFrame à partir d'un tableau simple à deux dimensions.
+     *
      * @param array Tableau d'objets à deux dimensions pouvant être convertis en dataFram. Les colonnes doivent donc avoir des types d'objet constant.
      * @throws MistypedRowException Quand le tableau rentré en argument est non convetible en dataFrame.
      */
@@ -27,23 +28,23 @@ public class Dataframe {
             this.rowType = new HashMap<>();
             this.indexLabel = new HashMap<>();
             //Parcours des colonnes
-            for(int j = 0; j < array[0].length; j++) {
+            for (int j = 0; j < array[0].length; j++) {
                 int value = j;
-                StringBuilder sb = new StringBuilder(String.valueOf((char)('A'+(value % 26))));
+                StringBuilder sb = new StringBuilder(String.valueOf((char) ('A' + (value % 26))));
                 //Boucle while générant des labels pour les colonnes en suivant l'ordre alphabétique.
-                while((value = (value/26-1)) >= 0)
-                    sb.append((char)('A'+(value % 26)));
+                while ((value = (value / 26 - 1)) >= 0)
+                    sb.append((char) ('A' + (value % 26)));
                 label = sb.reverse().toString();
                 this.frameRows.put(label, new ArrayList<>());
                 this.indexLabel.put(label, j);
                 Object firstElemRow = array[0][j];
                 this.rowType.put(label, Class.forName(firstElemRow.getClass().getName()));
                 //Parcours des lignes
-                for(int i = 0; i < array.length; i++) {
-                    if(!this.frameLines.containsKey(i))
+                for (int i = 0; i < array.length; i++) {
+                    if (!this.frameLines.containsKey(i))
                         this.frameLines.put(i, new ArrayList<>());
                     Object e = array[i][j];
-                    if(!(e.getClass().getName().equals(this.rowType.get(label).getName())))
+                    if (!(e.getClass().getName().equals(this.rowType.get(label).getName())))
                         throw new MistypedRowException();
                     else {
                         this.frameLines.get(i).add(array[i][j]);
@@ -52,7 +53,7 @@ public class Dataframe {
                 }
             }
         } catch (MistypedRowException | ClassNotFoundException e) {
-            if(e.getClass().getName().equals("pandaJava.MistypedRowException"))
+            if (e.getClass().getName().equals("pandaJava.MistypedRowException"))
                 throw new MistypedRowException("Mistyped array argument. A row has a unique type.", e);
             else
                 e.printStackTrace();
@@ -61,9 +62,10 @@ public class Dataframe {
 
     /**
      * Création d'un DataFrame à partir d'un fichier CSV. On considère qu'il ne peut y avoir que 3 types dans le fichier CSV : Integer, boolean et String.
+     *
      * @param path Chemin vers le fichier CSV à convertir en DataFrame.
      */
-    public Dataframe (String path) {
+    public Dataframe(String path) {
         try {
             this.frameRows = new HashMap<>();
             this.frameLines = new HashMap<>();
@@ -85,10 +87,10 @@ public class Dataframe {
                 //Deuxième ligne (labels)
                 else if (i == 1) {
                     labels = st.split(";");
-                    for(int j = 0; j < labels.length; j++) {
+                    for (int j = 0; j < labels.length; j++) {
                         this.frameRows.put(labels[j], new ArrayList<>());
                         this.indexLabel.put(labels[j], j);
-                        this.rowType.put(labels[j], Class.forName("java.lang."+types[j]));
+                        this.rowType.put(labels[j], Class.forName("java.lang." + types[j]));
                     }
                     i++;
                 }
@@ -97,15 +99,14 @@ public class Dataframe {
                 else {
                     this.frameLines.put(lineIndex, new ArrayList<>());
                     String[] line = st.split(";");
-                    for(int j = 0; j < line.length; j++) {
+                    for (int j = 0; j < line.length; j++) {
                         Class c = this.rowType.get(labels[j]); //On récupère la classe de la colonne
                         Object elem;
                         //On cast l'élément en String, en Int ou en booléen en fonction du type de la colonne.
-                        if(c.getName().equals("java.lang.Integer"))
+                        if (c.getName().equals("java.lang.Integer"))
                             elem = Integer.valueOf(line[j]);
-                        else
-                            if (c.getName().equals("java.lang.Boolean"))
-                                elem = Boolean.valueOf(line[j]);
+                        else if (c.getName().equals("java.lang.Boolean"))
+                            elem = Boolean.valueOf(line[j]);
                         else
                             elem = line[j];
                         this.frameLines.get(lineIndex).add(elem);
@@ -121,16 +122,18 @@ public class Dataframe {
 
     /**
      * Renvoie la valeur d'une cellule du DataFrame à un index et label donnés.
+     *
      * @param index Ligne de la cellule.
      * @param label Colonne de la cellule.
      * @return valeur de la cellule.
      */
-    public Object getValue (int index, String label) {
+    public Object getValue(int index, String label) {
         return this.frameLines.get(index).get(this.indexLabel.get(label));
     }
 
     /**
      * Renvoie la liste d'objets d'une colonne à partir de son label.
+     *
      * @param label Label de la colonne.
      * @return une liste d'objets composant la colonne sélectionnée.
      */
@@ -140,6 +143,7 @@ public class Dataframe {
 
     /**
      * Renvoie la liste d'objets d'une ligne à partir de son index.
+     *
      * @param index Index d'une ligne.
      * @return la liste d'objets composant la ligne sélectionnée.
      */
@@ -149,6 +153,7 @@ public class Dataframe {
 
     /**
      * Renvoie la classe des objets d'une colonne à partir de son label.
+     *
      * @param label Label d'une colonne.
      * @return la classe à laquelle appartiennent les objets de la colonne.
      */
@@ -159,21 +164,22 @@ public class Dataframe {
     /**
      * Renvoie en sous-ensemble de lignes en fonction de deux index. Les lignes renvoyées sont comprises dans l'intervalle index1 et index2 avec les bornes incluses.
      * Le premier index ne peut pas être plus grand que le deuxième.
+     *
      * @param index1 Index de la ligne correspondant à la borne inférieur de l'intervalle de lignes que l'on veut sélectionner.
      * @param index2 Index de la ligne correspondant à la borne supérieur de l'intervalle de lignes que l'on veut sélectionner.
      * @return sous ensemble de lignes.
      */
-    public Map<Integer, List<Object>> getSubLines (int index1, int index2) {
+    public Map<Integer, List<Object>> getSubLines(int index1, int index2) {
         Map<Integer, List<Object>> lines = new HashMap<>();
         try {
 
             //Cas où les index en paramètre ne sont pas valides.
-            if((index2 < index1) || (index1 < 0) || (index2 > this.frameLines.keySet().size()-1))
+            if ((index2 < index1) || (index1 < 0) || (index2 > this.frameLines.keySet().size() - 1))
                 throw new IllegalArgumentException();
 
             else {
-                for(int key : this.frameLines.keySet()) {
-                    if((key >= index1) && (key <= index2)) {
+                for (int key : this.frameLines.keySet()) {
+                    if ((key >= index1) && (key <= index2)) {
                         lines.put(key, this.frameLines.get(key));
                     }
                 }
@@ -191,10 +197,11 @@ public class Dataframe {
      * Renvoie un sous ensemble de lignes en fonction de booléens.
      * Chaque booléen passé en paramètre correspond à un index et on récupère la ligne si l'index correspondant est à true en paramètre.
      * Par exemple si le premier pramètre est à true, on garde la ligne d'index 0.
+     *
      * @param index Suite d'index correspondant à des lignes. La place du booléean correspond à l'index d'une ligne.
      * @return sous ensemble de lignes.
      */
-    public Map<Integer, List<Object>> getSubLines (boolean... index) {
+    public Map<Integer, List<Object>> getSubLines(boolean... index) {
         Map<Integer, List<Object>> lines = new HashMap<>();
         int key = 0;
         try {
@@ -202,8 +209,8 @@ public class Dataframe {
             if (index.length != this.frameLines.keySet().size())
                 throw new IllegalArgumentException();
             else {
-                for(int i = 0; i < index.length; i++) {
-                    if(index[i]) {
+                for (int i = 0; i < index.length; i++) {
+                    if (index[i]) {
                         lines.put(key, this.frameLines.get(i));
                         key++;
                     }
@@ -220,20 +227,20 @@ public class Dataframe {
     /**
      * Renvoie un sous ensemble de lignes en fonction d'un label et d'une expression lambda. Filte les lignes en fonction des valeurs d'une colonne.
      * Par exemple pour les paramètres : (age,(x) -&gt; x &gt; 5); on va récupérer toutes les lignes où l'âge est supérieur à 5.
-     * @param label Label correspondant à une colonne.
+     *
+     * @param label      Label correspondant à une colonne.
      * @param evaluation Expression booléenne évaluant la donnée d'une cellule du dataFrame. On peut par exemple sélectionner les valeurs x de la colonne pour x supérieur à 5.
      * @return sous ensemble de lignes.
      */
-    public Map<Integer, List<Object>> getSubLines (String label, Evaluate evaluation) {
+    public Map<Integer, List<Object>> getSubLines(String label, Evaluate evaluation) {
         Map<Integer, List<Object>> lines = new HashMap<>();
         try {
-            if(!this.rowType.get(label).getName().equals("java.lang.Integer")) {
+            if (!this.rowType.get(label).getName().equals("java.lang.Integer")) {
                 throw new IllegalArgumentException();
-            }
-            else {
+            } else {
                 int index = 0;
-                for(int i : this.frameLines.keySet()) {
-                    if(evaluation.evaluate((int)this.getValue(i, label))) {
+                for (int i : this.frameLines.keySet()) {
+                    if (evaluation.evaluate((int) this.getValue(i, label))) {
                         lines.put(index, this.frameLines.get(i));
                         index++;
                     }
@@ -250,18 +257,18 @@ public class Dataframe {
     /**
      * Renvoie en sous ensemble de colonnes en fonction de labels.
      * Toutes les colonnes qui ont un label correspondant à un de ceux en paramètre est ajoutée au sous-ensemble de retour.
+     *
      * @param label Suite de labels correspondant à des colonnes.
      * @return sous ensemble de colonnes.
      */
-    public Map<String, List<Object>> getSubRows (String... label) {
+    public Map<String, List<Object>> getSubRows(String... label) {
         Map<String, List<Object>> rows = new HashMap<>();
         try {
-            for(String l : label) {
+            for (String l : label) {
                 //Cas où au moins un des labels passé en paramètre n'existe pas.
                 if (!this.frameRows.keySet().contains(l)) {
                     throw new IllegalArgumentException();
-                }
-                else {
+                } else {
                     rows.put(l, this.frameRows.get(l));
                 }
             }
@@ -275,6 +282,7 @@ public class Dataframe {
     /**
      * Méthode permettant d'afficher un dataframe sur la console.
      * Pour le faire : System.out.println(myDataframe);
+     *
      * @return le string contenant l'affichage souhaité
      */
     public String toString() {
@@ -300,6 +308,7 @@ public class Dataframe {
 
     /**
      * Affiche les premières lignes d'un dataframe
+     *
      * @param nbLines indique le nombre de lignes à afficher
      * @return un string contenant l'affichage souhaité
      * @throws Exception Un mauvais nombre de lignes a été transmis
@@ -332,6 +341,7 @@ public class Dataframe {
 
     /**
      * Affiche les derniers lignes d'un dataframe
+     *
      * @param nbLines indique le nombre de lignes à afficher
      * @return un string contenant l'affichage souhaité
      * @throws Exception Un mauvais nombre de lignes a été transmis
@@ -370,8 +380,8 @@ public class Dataframe {
      * @param label Indique la colonne du dataframe
      * @return vrai si le label entré correspond bien à un label présent dans le dataframe
      */
-    public boolean labelIsCorrect(String label){
-        if(getRowType(label) == null){
+    public boolean labelIsCorrect(String label) {
+        if (getRowType(label) == null) {
             throw new IllegalArgumentException("Incorrect label parameter");
         }
         return true;
@@ -387,7 +397,7 @@ public class Dataframe {
         labelIsCorrect(label);
         int size = getRow(label).size();
         float columnSum = dataframeSum(label);
-        return (columnSum/size);
+        return (columnSum / size);
     }
 
     /**
@@ -428,14 +438,14 @@ public class Dataframe {
                 myValues.add(Float.valueOf((Integer) object));
             }
             return Collections.min(myValues);
-        } else if(getRowType(label) == String.class || getRowType(label) == Character.class)  {
+        } else if (getRowType(label) == String.class || getRowType(label) == Character.class) {
             List<Object> myObjects = getRow(label);
             List<String> myValues = new ArrayList<>();
             for (Object object : myObjects) {
                 myValues.add(String.valueOf(object));
             }
             return Collections.min(myValues);
-        }else{
+        } else {
             throw new IllegalArgumentException("The column contain unknown type values for min");
         }
     }
@@ -457,14 +467,14 @@ public class Dataframe {
                 myValues.add(Float.valueOf((Integer) object));
             }
             return Collections.max(myValues);
-        } else if(getRowType(label) == String.class || getRowType(label) == Character.class) {
+        } else if (getRowType(label) == String.class || getRowType(label) == Character.class) {
             List<Object> myObjects = getRow(label);
             List<String> myValues = new ArrayList<>();
             for (Object object : myObjects) {
                 myValues.add(String.valueOf(object));
             }
             return Collections.max(myValues);
-        }else{
+        } else {
             throw new IllegalArgumentException("The column contain unknown type values for max");
         }
     }
